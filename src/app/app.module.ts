8f97d9e5aa973ddb955febde0742480fd4734a81
ababacar gueye <sadikhabou@gmail.com>
2021-11-27 16:59:53 +0100

    création des composants dashboard(liste,detail et recherche)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListeComponent } from './dashboard/liste/liste.component';
+import { DetailComponent } from './dashboard/detail/detail.component';
+import { RechercheComponent } from './dashboard/recherche/recherche.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ListeComponent,
+    DetailComponent,
+    RechercheComponent
   ],
   imports: [
     BrowserModule,
