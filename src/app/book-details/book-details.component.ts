e9ccf312a942b51f1be878d88613b37e70762e5f
Jean-baptiste <jean-baptiste.sossou@gurulab.co>
2021-12-28 01:01:15 +0100

    add ajout-panier depuis la page de description de livre

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router'
 import { LivresService } from '../livres.service';
-
+import { PartageService } from '../partage.service';
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
@@ -11,12 +11,20 @@ import { LivresService } from '../livres.service';
 export class BookDetailsComponent implements OnInit {
   public livre : any; 
   livresService = new LivresService
-  constructor(livresService : LivresService, private route:ActivatedRoute ) {
+  public panier: any;
+  constructor(livresService : LivresService, private route:ActivatedRoute, private partageService:PartageService ) {
     this.livre = {}
   }
 
   async ngOnInit() {
     this.livre = await this.livresService.getLivre(this.route.snapshot.paramMap.get('id'));
+    this.partageService.panier.subscribe((panier: any) => this.panier = panier)
   }
+  
+  addToPanier(livre:any) {
+    this.panier.push(livre);
+    this.partageService.addTopanier(this.panier);
+    console.log(this.partageService.panier);
 
+  }
 }
